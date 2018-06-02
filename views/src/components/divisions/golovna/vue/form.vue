0004a744858dad91d4d2846a6c84b71c5c04f839
Denys Maletskiy <maletsden@gmail.com>
2018-06-02 21:04:42 +0300

    Make it responsible

--- views/src/components/divisions/golovna/vue/form.vue
+++ views/src/components/divisions/golovna/vue/form.vue
@@ -1,42 +1,48 @@
 <template>
 <div id="form">
-  <div>
-    квіти в дарунок - це те, що змушує нашу душу цвісти
-  </div>
-  <div id="form_header">
-    ОФОРМІТЬ ВАШЕ ЗАМОВЛЕННЯ ВЖЕ СЬОГОДНІ
-  </div>
-
-  <div class="row_with_inputs">
-    <input type="text" name="name" placeholder="Ім'я" />
-    <input type="text" name="type_of_shopping" placeholder="Вид покупки" />
-  </div>
-
-  <div class="row_with_inputs">
-    <input type="text" name="telephone" placeholder="Тел" />
-    <input type="text" name="email" placeholder="E-mail" />
-  </div>
-
-  <div id="large_input">
-    <textarea placeholder="Ваше повідомлення" />
-  </div>
-
-  <div id="bottom_text">
-    Залиште своє повідомлення і менеджер La Roserie зв’яжеться з Вами, щоб уточнити наявність квітів, кінцеву вартість замовленя та інші деталі
-  </div>
-
-  <div id="button_send">
-    Надіслати
-  </div>
-
-  <div id="commenting_div">
-    <div>
-      <icon name='commenting'></icon>
+  <div class="section_topic section_topic-white">
+    <div class="section_topic__small_text">
+      квіти в дарунок - це те, що змушує нашу душу цвісти
     </div>
-    <div>
-      Не можете визначитись із вибором?<br> Ми з радістю Вас <b>проконсультуємо!</b>
+    <div class="section_topic__main_text">
+      ОФОРМІТЬ ВАШЕ ЗАМОВЛЕННЯ ВЖЕ СЬОГОДНІ
     </div>
   </div>
+
+  <div class="basic_form">
+    <div class="basic_form__row_inputs">
+      <input type="text" name="name" placeholder="Ім'я" />
+      <input type="text" name="type_of_shopping" placeholder="Вид покупки" />
+    </div>
+
+    <div class="basic_form__row_inputs">
+      <input type="text" name="telephone" placeholder="Тел" />
+      <input type="text" name="email" placeholder="E-mail" />
+    </div>
+
+    <div class="basic_form__large_textarea">
+      <textarea placeholder="Ваше повідомлення" />
+    </div>
+
+    <div class="basic_form__bottom_text">
+      Залиште своє повідомлення і менеджер Fresh Flowers зв’яжеться з Вами, щоб уточнити наявність квітів, кінцеву вартість замовленя та інші деталі
+    </div>
+
+    <div class="basic_form__button_send">
+      Надіслати
+    </div>
+
+    <div class="basic_form__commenting_div">
+      <div>
+        <icon name='commenting'></icon>
+      </div>
+      <div>
+        Не можете визначитись із вибором?<br> Ми з радістю Вас <b>проконсультуємо!</b>
+      </div>
+    </div>
+
+  </div>
+
 
 </div>
 </template>
