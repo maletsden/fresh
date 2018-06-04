7017e819d6c40c3d35f8c0688063593414fdb83e
Denys Maletskiy <maletsden@gmail.com>
2018-06-04 23:09:37 +0300

    Created a one more version of we_offer.vue component

--- views/src/components/divisions/golovna/vue/we_offer.vue
+++ views/src/components/divisions/golovna/vue/we_offer.vue
@@ -1,60 +1,31 @@
 <template>
 <div id="we_offer">
-  <div class='semi_trensparent_bg' />
-  <div class="section_topic section_topic-purple">
-    <div class="section_topic__small_text">
-      Так само як і цілі гектари тюльпанів у Нідерландах, у нас Ви зможете скористатись широким спектром флористичних послуг
-    </div>
-    <div class="section_topic__main_text">
-      Ми пропонуємо вам:
-    </div>
-  </div>
 
   <div class="we_offer_row">
-    <div class="we_offer_row__block">
-      <div class="we_offer_row__image"></div>
-      <div class='we_offer_row__image_text'>
-        Великий вибір квітів <br> та рослин
-      </div>
-    </div>
-    <div class="we_offer_row__block">
-      <div class="we_offer_row__image"></div>
-      <div class='we_offer_row__image_text'>
-        Вишукані композиції <br> ручної роботи
-      </div>
-    </div>
-    <div class="we_offer_row__block">
-      <div class="we_offer_row__image"></div>
-      <div class='we_offer_row__image_text'>
-        Оформлення подарунків <br> для будь-якої події
-      </div>
-    </div>
+    <div class="we_offer_row__img_bouquet">
 
-    <div class="we_offer_row__block">
-      <div class="we_offer_row__image"></div>
-      <div class='we_offer_row__image_text'>
-        Онлайн-консультація <br> флориста
-      </div>
     </div>
     <div class="we_offer_row__block">
-      <div class="we_offer_row__image"></div>
-      <div class='we_offer_row__image_text'>
-        Доставка квітів <br> додому
+      <div class="we_offer_row__block_topic">Великий вибір<br>квітів та композицій</div>
+      <div class="we_offer_row__block_topic_decription">
+        Якщо ви збираєтеся на святкування або ж просто хочете сповнити ваш день частинкою світла - завітайте до Fresh Flowers.
       </div>
-    </div>
-    <div class="we_offer_row__block">
-      <div class="we_offer_row__image"></div>
-      <div class='we_offer_row__image_text'>
-        Приємні ціни та <br> вигідні ціни
+      <div class="we_offer_row__block_we_offer">
+        Ми пропонуємо:
       </div>
+      <ul class="we_offer_row__block_ul">
+        <li>Букети</li>
+        <li>Квіти</li>
+        <li>Декорація подарунків</li>
+      </ul>
     </div>
-
   </div>
+  <div class="we_offer_row">
+    <div class="we_offer_row__block">
 
+    </div>
+    <div class="we_offer_row__img_wedding_bouquet">
 
-  <div id="to_know_more">
-    <div>
-      Дізнатися більше
     </div>
   </div>
 </div>
---
+++ views/src/components/divisions/golovna/vue/we_offer1.vue
@@ -0,0 +1,76 @@
+<template>
+<div id="we_offer">
+  <div class='semi_trensparent_bg' />
+  <div class="section_topic section_topic-purple">
+    <div class="section_topic__small_text">
+      Так само як і цілі гектари тюльпанів у Нідерландах, у нас Ви зможете скористатись широким спектром флористичних послуг
+    </div>
+    <div class="section_topic__main_text">
+      Ми пропонуємо вам:
+    </div>
+  </div>
+
+  <div class="we_offer_row">
+    <div class="we_offer_row__block">
+      <div class="we_offer_row__image"></div>
+      <div class='we_offer_row__image_text'>
+        Великий вибір квітів <br> та рослин
+      </div>
+    </div>
+    <div class="we_offer_row__block">
+      <div class="we_offer_row__image"></div>
+      <div class='we_offer_row__image_text'>
+        Вишукані композиції <br> ручної роботи
+      </div>
+    </div>
+    <div class="we_offer_row__block">
+      <div class="we_offer_row__image"></div>
+      <div class='we_offer_row__image_text'>
+        Оформлення подарунків <br> для будь-якої події
+      </div>
+    </div>
+
+    <div class="we_offer_row__block">
+      <div class="we_offer_row__image"></div>
+      <div class='we_offer_row__image_text'>
+        Онлайн-консультація <br> флориста
+      </div>
+    </div>
+    <div class="we_offer_row__block">
+      <div class="we_offer_row__image"></div>
+      <div class='we_offer_row__image_text'>
+        Доставка квітів <br> додому
+      </div>
+    </div>
+    <div class="we_offer_row__block">
+      <div class="we_offer_row__image"></div>
+      <div class='we_offer_row__image_text'>
+        Приємні ціни та <br> вигідні ціни
+      </div>
+    </div>
+
+  </div>
+
+
+  <div id="to_know_more">
+    <div>
+      Дізнатися більше
+    </div>
+  </div>
+
+</div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {};
+  },
+  mounted() {},
+  components: {}
+}
+</script>
+<style lang='scss' scoped>
+@import '../scss/we_offer1.scss';
+</style>
