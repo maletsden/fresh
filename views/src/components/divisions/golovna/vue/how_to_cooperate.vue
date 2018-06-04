c3716946cca783587a84fadf083e9e47b8325045
Denys Maletskiy <maletsden@gmail.com>
2018-06-04 23:08:34 +0300

    Made it responsive and rewrote scss due to BEM

--- views/src/components/divisions/golovna/vue/how_to_cooperate.vue
+++ views/src/components/divisions/golovna/vue/how_to_cooperate.vue
@@ -1,15 +1,17 @@
 <template>
-<div id='main'>
-  <div/>
-  <div>
-    Аби не натрапити на “кота у мішку”, наш пріоритет -<br> прозорість процесу замовлення і допомога з доставкою
-  </div>
-  <div id="header">
-    ЯК МИ ІЗ ВАМИ СПІВПРАЦЮЄМО:
+<div id='how_to_cooperate'>
+  <div class='semi_trensparent_bg'/>
+  <div class="section_topic">
+    <div class="section_topic__small_text">
+      Аби не натрапити на “кота у мішку”, наш пріоритет -<br> прозорість процесу замовлення і допомога з доставкою
+    </div>
+    <div class="section_topic__main_text section_topic__main_text-bold section_topic__main_text-smaller">
+      ЯК МИ ІЗ ВАМИ СПІВПРАЦЮЄМО:
+    </div>
   </div>
 
   <div id='flower_block' class='flower_block_show'>
-    <div id='central_circles'>
+    <div id='flower_block__central_circles'>
       <div/>
       <div/>
       <div/>
@@ -17,34 +19,32 @@
       <div/>
     </div>
 
-    <div id="first_block">
+    <div id="flower_block__first_block" class='circle'>
       <div/>
       <img/>
       <div>1</div>
       <p>Обираєте квітку або букет</p>
     </div>
-    <div id="second_block">
+    <div id="flower_block__second_block" class='circle'>
       <div/>
       <img/>
       <img/>
       <div>2</div>
       <p>Якщо потрібно, консультуєтеся із нашим флористом</p>
     </div>
-    <div id="third_block">
+    <div id="flower_block__third_block" class='circle'>
       <div/>
       <img/>
       <div>3</div>
       <p>Оформлюєте онлайн замовлення</p>
     </div>
-    <div id="forth_block">
+    <div id="flower_block__forth_block" class='circle'>
       <div/>
       <img/>
       <div>4</div>
       <p>Отримуєте квіти за вказаною адресою</p>
     </div>
   </div>
-  <div id="padding"></div>
-
 </div>
 </template>
 
