c4d11a3a06d7e26b92b6ba35306950d92ad11fad
Denys Maletskiy <maletsden@gmail.com>
2018-06-04 23:06:34 +0300

    Changed the structure of the Footer

--- views/src/components/Footer.vue
+++ views/src/components/Footer.vue
@@ -1,23 +1,118 @@
 <template>
-<div>
+<div id='Footer'>
 
+  <div id="footer_row">
+
+    <div class="footer_block">
+      <div class="footer_block__topic">
+        КОНТАКТИ
+      </div>
+
+      <div class="footer_block__row">
+        <icon name='mobile-phone' class='footer_block__icon'></icon>
+        <div class='footer_block__text'>
+          + 38 (067) 80 00 797
+        </div>
+
+      </div>
+
+      <div class="footer_block__row">
+        <icon name='map-marker' class='footer_block__icon'></icon>
+        <div class='footer_block__text'>
+          + 38 (067) 80 00 797
+        </div>
+      </div>
+
+      <div class="footer_block__row">
+        <icon name='map-marker' class='footer_block__icon'></icon>
+        <div class='footer_block__text'>
+          + 38 (067) 80 00 797
+        </div>
+      </div>
+
+    </div>
+
+    <div class="footer_block">
+      <div class="footer_block__topic">
+        ПОСЛУГИ
+      </div>
+
+      <div class="footer_block__row">
+        <icon name='mobile-phone' class='footer_block__icon'></icon>
+        <div class='footer_block__text'>
+          + 38 (067) 80 00 797
+        </div>
+
+      </div>
+
+      <div class="footer_block__row">
+        <icon name='map-marker' class='footer_block__icon'></icon>
+        <div class='footer_block__text'>
+          + 38 (067) 80 00 797
+        </div>
+      </div>
+
+      <div class="footer_block__row">
+        <icon name='map-marker' class='footer_block__icon'></icon>
+        <div class='footer_block__text'>
+          + 38 (067) 80 00 797
+        </div>
+      </div>
+
+
+    </div>
+
+    <div class="footer_block footer_block-last">
+      <div class="footer_block__topic">
+        МИ В СОЦМЕРЕЖАХ
+      </div>
+
+      <div class="footer_block__row">
+        <icon name='mobile-phone' class='footer_block__icon'></icon>
+        <div class='footer_block__text'>
+          + 38 (067) 80 00 797
+        </div>
+
+      </div>
+
+      <div class="footer_block__row">
+        <icon name='map-marker' class='footer_block__icon'></icon>
+        <div class='footer_block__text'>
+          + 38 (067) 80 00 797
+        </div>
+      </div>
+
+      <div class="footer_block__row">
+        <icon name='map-marker' class='footer_block__icon'></icon>
+        <div class='footer_block__text'>
+          + 38 (067) 80 00 797
+        </div>
+      </div>
+
+
+    </div>
+  </div>
 </div>
 </template>
 
 
 <script>
+import 'vue-awesome/icons/mobile-phone';
+import 'vue-awesome/icons/map-marker';
+import Icon from 'vue-awesome/components/Icon.vue';
+
 export default {
   data() {
-    return {
-    };
+    return {};
   },
-  mounted() {
-  },
+  mounted() {},
   components: {
+    Icon
   }
 
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import 'Footer.scss';
 </style>
